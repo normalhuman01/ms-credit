c43325774b4cb9d57d64d1f559bbd1e929fdb5f1
NormalHuman <153409490+normalhuman01@users.noreply.github.com>
2023-12-15 13:50:17 -1200

    Add files via upload

---
+++ src/main/java/pe/com/project1/ms/MsCreditApplication.java
@@ -0,0 +1,13 @@
+package pe.com.project1.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCreditApplication.class, args);
+	}
+
+}
